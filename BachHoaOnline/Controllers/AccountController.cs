1d466a8fa685588132b52da0665864b41b829e5a
vnqmai <vnqmai01@gmail.com>
2019-08-08 19:25:26 +0700

    Account Management

--- BachHoaOnline/Controllers/AccountController.cs
+++ BachHoaOnline/Controllers/AccountController.cs
@@ -16,23 +16,48 @@ namespace BachHoaOnline.Controllers
             return View(db.Khachhang.ToList());
         }
 
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult DoCreate(Khachhang kh)
+        {
+            db.Khachhang.Add(kh);
+            ViewBag.status = "Tạo mới thành công.";
+            return View("Create");
+        }
+
         public IActionResult Update(int id)
         {
             return View(db.Khachhang.Where(x => x.Makh == id).SingleOrDefault());
         }
 
-        public IActionResult DoUpdate()
+        public IActionResult DoUpdate(Khachhang kh)
         {
-            return View("Update");
+            Khachhang des = db.Khachhang.Where(x => x.Makh == kh.Makh).SingleOrDefault();
+            des.Hoten = kh.Hoten;
+            des.Matkhau = kh.Matkhau;
+            des.Gioitinh = kh.Gioitinh;
+            des.Ngaysinh = kh.Ngaysinh;
+            des.Diachi = kh.Diachi;
+            des.Dienthoai = kh.Dienthoai;
+            des.Randomkey = kh.Randomkey;
+            db.SaveChanges();
+            ViewBag.status = "Cập nhật thành công.";
+            return View("Update",des);
         }
 
         public IActionResult Delete(int id)
-        {
+        {            
             return View(db.Khachhang.Where(x => x.Makh == id).SingleOrDefault());
         }
 
-        public IActionResult DoDelete()
+        public IActionResult DoDelete(Khachhang kh)
         {
+            db.Khachhang.Remove(db.Khachhang.Where(x => x.Makh == kh.Makh).SingleOrDefault());
+            db.SaveChanges();
+            ViewBag.status = "Đã xóa thành công.";
             return View("Delete");
         }
     }
